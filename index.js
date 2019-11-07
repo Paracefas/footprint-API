09676d8e221f717a2cf5914bf4b96331c3d0f249
Emanuel Clur <emanuelclur2001@outlook.com>
2019-11-07 10:15:19 -0300

    map view marker and popup

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const path = require('path')
 const dbConnection = require('./dbConnection')
+const cors = require('cors')
 
 const app = express()
 
 app.set('port', process.env.PORT || 27015)
 
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
@@ -16,6 +18,14 @@ app.set('view engine', 'ejs');
 app.get('/', (req, res) =>
 {
     res.render('pages/index')
+})
+
+app.get('/api/get-data', (req, res) =>
+{
+    dbConnection().query('SELECT * FROM Sensors', (err, result) =>
+    {
+        res.json({result})
+    })
 })
 
 app.post('/api/data', (req, res) => 
